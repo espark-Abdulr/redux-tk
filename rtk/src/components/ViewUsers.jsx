daf7883ff62773b1a95171279621f878476771df
espark-Abdulr <abdulrehman@esparkconsultants.com>
2023-09-27 20:43:58 +0500

    Display Done

--- rtk/src/components/ViewUsers.jsx
+++ rtk/src/components/ViewUsers.jsx
@@ -3,9 +3,58 @@ import { useSelector } from "react-redux";
 
 const ViewUsers = () => {
   const data = useSelector((c) => {
-    console.log(c.UserData.users);
+    return(c.UserData.users)
   });
-  return <div></div>;
+  return (
+    <>
+      {console.log(data)}
+      <table className="table-dark" style={{ width: "100vw" }}>
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Age</th>
+            <th scope="col">Edit option</th>
+            <th scope="col">Delete Option</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((eachUser, index) => (
+            <tr key={index} >
+              <th scope="row">{eachUser.id}</th>
+              <td>{eachUser.name}</td>
+              <td>{eachUser.age}</td>
+              <td>
+                <button type="button" className="btn btn-danger">
+                  Delete
+                </button>
+              </td>
+              <td>
+                <button type="button" className="btn btn-info">
+                  Edit
+                </button>
+              </td>
+            </tr>
+          ))}
+          {/* <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>21</td>
+            <td>
+              <button type="button" className="btn btn-danger">
+                Danger
+              </button>
+            </td>
+            <td>
+              <button type="button" className="btn btn-info">
+                Info
+              </button>
+            </td>
+          </tr> */}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default ViewUsers;
